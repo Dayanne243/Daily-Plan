b504c501bf048fab66d6a3af4f7350dead5d4a98
Dayanne Alves Brito <dada.alvesbrito24@gmail.com>
2025-11-19 09:25:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Lida com o formulário de login (se existir na página atual)
+
     const loginForm = document.getElementById('login-page');
     if (loginForm) {
         loginForm.addEventListener('submit', (e) => {
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const email = document.getElementById('login-email').value;
             const password = document.getElementById('login-password').value;
 
-            // Simulação de validação de login
+            // Simulação validação de login
             if (email === 'teste@gmail.com' && password === '12345') {
                 alert('Login bem-sucedido!');
                 window.location.href = 'agenda.html';
@@ -19,11 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Lida com o formulário de registro (se existir na página atual)
+
     const registerForm = document.getElementById('register-page');
     if (registerForm) {
         registerForm.addEventListener('submit', (e) => {
-            e.preventDefault(); // Impede o envio padrão do formulário
+            e.preventDefault();
 
             const username = document.getElementById('register-username').value;
             const email = document.getElementById('register-email').value;
@@ -33,7 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('Usuário registrado:', { username, email, password });
             alert('Registro bem-sucedido! Agora faça o login!.');
 
-            // Redireciona para a página de login após o registro
             window.location.href = 'index.html';
         });
     }
